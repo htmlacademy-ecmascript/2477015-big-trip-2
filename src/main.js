46d49e22f342415e952744411205cb878111a58a
Elizaveta Rumyanceva <elizaveta.rumyanceva@mail.ru>
2024-07-04 23:50:17 +0300

    build: установит dayjs

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import TripPresenter from '../presenter/trip-presenter.js';
+import TripPresenter from './presenter/trip-presenter.js';
 
 const pageMainElement = document.querySelector('.page-body__page-main');
 const tripPresenter = new TripPresenter({listContainer: pageMainElement});
